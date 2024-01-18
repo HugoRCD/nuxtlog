6fc5ba5fb88d308d14846e4a8b0a3872f411124a
Hugo Richard <hugo.richard@epitech.eu>
2024-01-18 15:22:35 +0100

    Initial commit

---
+++ nuxt.config.ts
@@ -0,0 +1,6 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  modules: ['fastnuxt'],
+  css: ['~/main.css'],
+  devtools: { enabled: true }
+})
---
+++ pages/index.vue
@@ -0,0 +1,12 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="flex justify-center items-center gap-3 h-screen">
+    <div class="text-4xl font-bold">
+      Hello World!
+    </div>
+    <ThemeToggle />
+  </div>
+</template>
+
+<style scoped lang="scss"></style>
